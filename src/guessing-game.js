77af87015129a84a018b51418b1f2bf64ffb1992
“Yulya <yulya_kevra@mail.ru>
2023-01-26 16:58:19 +0300

    feat:add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
+        
+
     constructor() {}
 
     setRange(min, max) {
-
+this.start=min;
+this.end=max;
     }
 
     guess() {
-
+this.mid=Math.round((this.start+this.end)/2);
+return this.mid
     }
 
     lower() {
-
+this.end=this.guess();
+return this.end;
     }
 
     greater() {
-
+this.start=this.guess();
+return this.start
     }
+
+
 }
 
 module.exports = GuessingGame;
